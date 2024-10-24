edd82096c88a028f80c321c4ba2b0de796cb3790
Umesh <umeshpal8141@gmail.com>
2024-10-24 18:28:26 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// ------------------- Hero section Buttons --------------------
+document.getElementById("myButton1").onclick = function (){
+    location.href = "./navigation/1product.html#mirrorless"
+}
+document.getElementById("myButton2").onclick = function (){
+    location.href = "./navigation/1product.html#dslr"
+}
+document.getElementById("myButton3").onclick = function (){
+    location.href = "./navigation/1product.html#film"
+}
+document.getElementById("myButton4").onclick = function (){
+    location.href = "./navigation/1product.html#action"
+}
+document.getElementById("myButton5").onclick = function (){
+    location.href = "./navigation/1product.html#drones"
+}
