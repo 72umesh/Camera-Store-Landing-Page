9f77a4142b40b825db5c268a18b88617121f7f82
72umesh <127204670+72umesh@users.noreply.github.com>
2024-10-25 14:10:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 
 // ------------------- Hero section Buttons --------------------
 document.getElementById("myButton1").onclick = function (){
-    location.href = "./navigation/1product.html#mirrorless"
+    location.href = "./Navigation/1product.html#mirrorless"
 }
 document.getElementById("myButton2").onclick = function (){
-    location.href = "./navigation/1product.html#dslr"
+    location.href = "./Navigation/1product.html#dslr"
 }
 document.getElementById("myButton3").onclick = function (){
     location.href = "./Navigation/1product.html#film"
 }
 document.getElementById("myButton4").onclick = function (){
-    location.href = "./navigation/1product.html#action"
+    location.href = "./Navigation/1product.html#action"
 }
 document.getElementById("myButton5").onclick = function (){
-    
-    const url ='https://72umesh.github.io/Camera-Store-Landing-Page/navigation/1product.html#drones'
-    console.log(url);
-    location.href = url;
+    location.href ="./Navigation/1product.html#drones"
 }
